ebfac740084e98e9b07d658ccbb7a4f0a0631a80
Diogo Antunes <diogoalves2684@gmail.com>
2024-12-01 23:54:51 -0300

    Teste pull

--- adote/models.py
+++ adote/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
 
-
+#TESTE // DIOGO :D
 class Usuario(AbstractUser):
     DOADOR = 'doador'
     ADOTANTE = 'adotante'
